e4206e33f317302ceb7c6f3f85daa9b1d68b7d2a
duran-andrew <andrewduran930@gmail.com>
2025-05-07 22:07:42 -0700

    updated prop info steps

--- python.py
+++ python.py
@@ -3,17 +3,19 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 property_info_steps_text = [ # Step 1 
-                            "If you’re redeeming a vehicle, contact your lender and confirm that a “Vehicle Release” has been issued directly to Paramount Recovery Service. This vehicle release MUST be issued to Paramount Recovery Service, directly. Otherwise, we cannot release the vehicle. No release is required for a property redemption appointment." ,
+                            "Schedule a property redemption appointment on the date and time of your choosing using the link below. Appointments must be scheduled within a days advance due to the volume of appointments trying to be made so schedule EARLY. Monday - Friday 10A.M. - 4P.M.. Appointments made after 4P.M. may not be confirmed until the following business day." ,
                             # Step 2
-                            "Schedule a vehicle or property redemption appointment on the date and time of your choosing using the button below. Once scheduled, we review your file, confirm fees, and anything else needed to book your release. Availability varies based on the volume of other appointments, so book early. Scheduling the appointment is not a confirmation. The confirmation response will take up to 3 hours, Monday through Friday. All appointments are confirmed or denied with an explanation by 5pm Monday through Friday. Appointments made after 5pm may not be confirmed until the following business day." , 
+                            "Once you schedule an appointment our office will review it and confirm with you any fees through text. If you are not the registered owner to the vehicle please call our office to assist you in redeeming your belongings." , 
                             # Step 3
-                            "Please make sure to include your cell phone number or email. We need your preferred contact method to communicate with you and provide the address, fees, and all other needed items. Once your appointment is scheduled, we start by reviewing your file and confirming your appointment by email or text message (based on your preference). Next, we confirm any fees owed, provide you with the address of the appointment, and provide you with the police department to contact to obtain your Police Release." , 
+                            "If you have any fees they must be paid in cash & must be exact change as the yard personnel are not allowed to carry cash on them." , 
                             # Step 4
-                            "For the security and safety and of our customers, visitors and employees, and in compliance with CDC recommendations, that encourage contactless payments to help curb the spread of COVID-19, we can not accept any cash payments at any of our facilities. Payments made at the time of your appointment will require a MICROCHIPPED card bearing either a “VISA”, “DISCOVER”, “MASTERCARD,” or “AMERICAN EXPRESS” logo. If you do not have a microchipped card with one of these logos you can purchase one at many major retailers prior to your arrival at our facility" , 
+                            "Please bring a valid driver's license to your appointment. A passport or militay ID is also acceptable. Contact our office if you are unable to provide a valid form of ID." , 
                             # Step 5
-                            "A government issued photo ID is acceptable in place of a Drivers license for a property redemption appointment. If you do not have a valid driver's license or a government issued photo ID, please call for assistance." ,
+                            "Please arrive at your appointment on time. If you are late, you may have to reschulde your appointment or wait for the next available appointment. If you are unable to make your appointment please call our office to reschedule." ,
                             # Step 6
-                            "If you are only picking up your property, please bring your keys so not to incur any additional fees from your lender."]
+                            "When you arrive to the yard to redeem your property we will have everything bagged up and ready to go for you when you arrive. We can not allow unauthorized personnel on to our yards for safety purposes.",
+                            # Step 7
+                            "Lastly, if you wish to surrender your key you can do so with the yard staff."]
 
 property_info_steps_1 = dict( step_name="Contacting your lender (VEHICLES ONLY)" , step_number=1 , step_text=property_info_steps_text[0])
 property_info_steps_2 = dict( step_name="Schedule an appointment" , step_number=2 , step_text=property_info_steps_text[1])
@@ -21,8 +23,9 @@ property_info_steps_3 = dict( step_name="Include phone and email" , step_number=
 property_info_steps_4 = dict( step_name="Payment (CASH ONLY & EXACT CHANGE)" , step_number=4 , step_text=property_info_steps_text[3])
 property_info_steps_5 = dict( step_name="Bring valid driver's license" , step_number=5 , step_text=property_info_steps_text[4])
 property_info_steps_6 = dict( step_name="Bring your keys" , step_number=6 , step_text=property_info_steps_text[5])
+property_info_steps_7 = dict( step_name="Surrender your keys" , step_number=7 , step_text=property_info_steps_text[6])
 
-messages = ["message-1" , "message-2" , "message-3" , "message-4" , "message-5" , "message-6"]
+messages = ["message-1" , "message-2" , "message-3" , "message-4" , "message-5" , "message-6", "message-7"]
 
 @app.route('/')
 def index():
@@ -48,11 +51,13 @@ def property_info():
     messages[3] = property_info_steps_4['step_text']
     messages[4] = property_info_steps_5['step_text']
     messages[5] = property_info_steps_6['step_text']
+    messages[6] = property_info_steps_7['step_text']
+    # You can add more messages or data as needed
 
     prop_url = "https://jasonnationsrecovery.wixsite.com/mysite/general-4"
 
 
-    return render_template('property_information.html', message=info_message, title=title, header=header, message_1=messages[0], message_2=messages[1], message_3=messages[2], message_4=messages[3], message_5=messages[4], message_6=messages[5], prop_url=prop_url)
+    return render_template('property_information.html', message=info_message, title=title, header=header, message_1=messages[0], message_2=messages[1], message_3=messages[2], message_4=messages[3], message_5=messages[4], message_6=messages[5], message_7=messages[6], prop_url=prop_url)
 
 
 if __name__ == '__main__':
